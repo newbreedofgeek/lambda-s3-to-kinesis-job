fec625366c6b1da8e870535a80516ba149c5fd5f
newbreedofgeek <paul.mark@gmail.com>
2018-03-28 10:14:52 +1100

    base code

--- handler.js
+++ handler.js
@@ -1,1 +1,47 @@
-// Code goes here
+from __future__ import print_function
+
+import urllib
+import boto3
+import io
+import gzip
+import json
+import uuid
+import datetime
+import base64
+
+s3 = boto3.client('s3')
+kinesis_client = boto3.client('kinesis', region_name='YOUR-REGION-HERE')
+bucket = 'YOUR-INPUT-BUCKET-HERE'
+stream = 'YOUR-OUTPUT-STREAM-HERE'
+
+def lambda_handler(event, context):
+    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+
+    try:
+        obj = s3.get_object(Bucket=bucket, Key=key)
+        bytestream = io.BytesIO(obj['Body'].read())
+        got_text = gzip.GzipFile(None, 'rb', fileobj=bytestream).read().decode('utf-8')
+        lines = got_text.splitlines()
+        pubs = []
+
+        for line in lines:
+          // TODO implement
+
+        for pub in pubs:
+            put_to_stream(pub)
+
+        print('%s items pushed to kinesis.'%(len(pubs)))
+
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
+
+    return 'Done'
+
+
+def put_to_stream(payload):
+    put_response = kinesis_client.put_record(
+                        StreamName=stream,
+                        Data=json.dumps(payload),
+                        PartitionKey=payload['partitionkey'])
